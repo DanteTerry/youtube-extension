913a049c4a5463fabf18c50bf48dd25add0c49be
DanteTerry <sinasa88@gmail.com>
2024-09-15 23:11:52 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h1 className="text-4xl font-medium"> Hello from youtube extension</h1>
+    </div>
+  );
+}
+export default App;
